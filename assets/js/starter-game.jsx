3b79e35c02af55165be0e3a501988ccb2080f290
Hemanth Nhs <hemanthsai10@gmail.com>
2019-09-26 03:53:29 -0400

    Basic table structure created

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -3,45 +3,40 @@ import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
 export default function game_init(root) {
-  ReactDOM.render(<Starter />, root);
+    ReactDOM.render(<Starter/>, root);
 }
 
 class Starter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { left: false };
-  }
-
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
-  }
-
-  hax(_ev) {
-    alert("hax!");
-  }
-
-  render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
-
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
+    constructor(props) {
+        super(props);
+        this.state = {left: false};
     }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
+
+    reset(_ev) {
+        // TODO
+        alert("TODO Game Reset");
     }
-  }
+
+    render() {
+        let button = <span><button onClick={this.reset.bind(this)}>Reset Game</button></span>
+        const grid_rows = []
+        for (let row = 1; row <= 4; row++) {
+            let col_grids = []
+            for (let col = 1; col <= 4; col++) {
+                col_grids.push(<td key={row * col} className="column column-10 tile"></td>)
+            }
+            grid_rows.push(<tr key={row} className="row">{col_grids}</tr>)
+        }
+        let tiles = <div>
+            <table className="container">
+                <tbody>{grid_rows}</tbody>
+            </table>
+        </div>;
+
+        return <div>
+            {tiles}
+            {button}
+        </div>;
+    }
 }
 
