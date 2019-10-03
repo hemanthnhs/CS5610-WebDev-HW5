a9e6a8aaac2ec013d4ec8f123c449452de1590bc
Hemanth Nhs <hemanthsai10@gmail.com>
2019-10-03 04:13:08 -0400

    Basic server side functionality implemented

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -6,20 +6,6 @@ export default function game_init(root, channel) {
     ReactDOM.render(<Starter channel={channel}/>, root);
 }
 
-// State structure:
-// {
-//    0:{
-//      leter:'E',      //Letter stored at tile 1
-//      status:'hide',  //Status of the tile hide=>not active, active=> displayed but not yet matched, completed=>matched
-//      count:0         //Number of times this tile is clicked
-//     },
-//    1:{},             //Tile 2
-//      ...
-//      ...
-//      ...,
-//     15:{}            //Tile 16
-// }
-
 class Starter extends React.Component {
     //Constructor method for initializing the state
     constructor(props) {
@@ -27,41 +13,21 @@ class Starter extends React.Component {
         this.channel = props.channel;
         //Generating tiles data and initializing tile properties
         //{0:{letter:'D',status:'hide',count:0},1:{...},....}
-        // this.state = {ss: tilesGenerated, active_tiles: [], completed_tiles: [], clicks: 0};
-        this.state = {tiles: [], active_tiles: [], completed_tiles: [], clicks: 0};
+        this.state = {tiles: [], active_tiles: [], completed_tiles: [], clicks: 0, gameStatus: 0};
         // Attribution http://ccs.neu.edu/home/ntuck/courses/2019/09/cs5610/notes/05-react/
         this.handleClick = this.handleClick.bind(this);
 
         this.channel
             .join()
             .receive("ok", this.got_view.bind(this))
-            .receive("error", resp => { console.log("Unable to join", resp); });
+            .receive("error", resp => {
+                console.log("Unable to join", resp);
+            });
     }
 
     got_view(view) {
         console.log("new view", view);
         this.setState(view.tile);
-    }
-
-    //This method serves the purpose of updating any tile detail i.e changing state based on given params
-    //data : state object, ids : ids of tiles to be modified, status : status with which tiles should be updated
-    //incrementCount : to increment or not increment count
-    updateTiles(data, ids, status, incrementCount = false) {
-        //Iterating through provided ids
-        _.map(ids, function (id) {
-            //Updating status of given ids with given state
-            data[id]['status'] = status
-            //For complete this is will result in additional score point to prevent it we have this condition
-            if (status != 'complete' && incrementCount) {
-                data[id]['count']++
-            }
-        })
-        //Handling for complete case
-        if (status == 'complete' && incrementCount) {
-            data[ids[1]]['count']++
-        }
-        //Updating the state with updated information
-        this.setState({tileData: data})
     }
 
     //This method is to handle the click of tiles. Based on the state of game, the new state will be generated here.
@@ -78,16 +44,26 @@ class Starter extends React.Component {
 
     //Utility to get game status
     fetchGameStatus(STATUS) {
-        //Get the number of tiles completed
-        let completedTiles = _.filter(this.state.tileData, {'status': 'complete'})
         //Game 0 implies not started case
-        if (this.getScore() == 0) {
+        if (this.state.gameStatus == 0) {
             return STATUS.BEGIN;
-        } else if (completedTiles.length == 16) {
+        } else if (this.state.gameStatus == 2) {
             //All tiles matched case
             return STATUS.COMPLETE;
         }
         return STATUS.INPROGRESS;
+    }
+
+    //Utility for score rendering area. Not to display before the game begin
+    renderScoreArea(gameStatus, STATUS) {
+        if (gameStatus != STATUS.BEGIN) {
+            //When game completes
+            let scoreLabel = (gameStatus == STATUS.COMPLETE) ? "Final Score" : "Score"
+            let score = <span
+                className="column column-100"><h1><u>{scoreLabel}</u></h1><h2>{this.state.clicks}</h2></span>;
+            return score
+        }
+        return <span></span>
     }
 
     // Render utility to generate tiles
@@ -104,15 +80,54 @@ class Starter extends React.Component {
                 let id = grid_ind++
                 // To assign class based on state
                 // Useful for styling
-                col_grids.push(<td key={id} id={id} className={'column column-25 tile hide'}
+                let classVar = "hide"
+                if (this.state.active_tiles.includes(id)) {
+                    classVar = "tile-active"
+                } else if (this.state.completed_tiles.includes(id)) {
+                    classVar = "tile-complete"
+                }
+                col_grids.push(<td key={id} id={id} className={'column column-25 tile ' + classVar}
                                    onClick={() => {
                                        this.handleClick(id)
-                                   }}>{id in this.state.active_tiles
-                    ? this.state.active_tiles[id] : ''}</td>)
+                                   }}>{(this.state.active_tiles.includes(id) || this.state.completed_tiles.includes(id))
+                    ? this.state.tiles[id - 1] : ''}</td>)
             }
             grid_rows.push(<tr key={row} className="row">{col_grids}</tr>)
         }
         return grid_rows
+    }
+
+    //Utility to get the game instruction/reset/restart button based on game status
+    renderInstructionSection(gameStatus, STATUS) {
+        if (gameStatus == STATUS.BEGIN) {
+            return <span className="game-instruction">Click on any tile to begin the game</span>
+        } else {
+            let buttonLabel = (gameStatus == STATUS.COMPLETE) ? " Restart Game " : "  Reset  Game  "
+            let button = <button className="gameplay-btn" onClick={this.reset.bind(this)}>{buttonLabel}</button>
+            return button
+        }
+    }
+
+    renderGameInfo() {
+        //enum variable for game status
+        const STATUS = {
+            BEGIN: "Yet to Start..",
+            INPROGRESS: "In Progress...",
+            COMPLETE: "YOU WIN!!!!"
+        }
+        //Get game status
+        let gameStatus = this.fetchGameStatus(STATUS);
+        let status = <span
+            className="column column-100"><h1><u>Game Status</u></h1><h2>{gameStatus}</h2></span>;
+        let score = this.renderScoreArea(gameStatus, STATUS)
+        let button = this.renderInstructionSection(gameStatus, STATUS);
+
+        let instructions = <span className="column column-100"
+                                 id="instructions-section">{button}</span>
+        let gameInfo = <span className="column column-50 column-offset-10">
+                            {status}{score}{instructions}
+                        </span>
+        return gameInfo
     }
 
     //Render
@@ -124,6 +139,7 @@ class Starter extends React.Component {
             </table>
         </span>;
         let header = <div id="game-heard" className="column column=100">Memory Game</div>
+        let gameInfoDisplay = this.renderGameInfo()
         return <div>
             <div className="row">
                 {header}
@@ -132,6 +148,7 @@ class Starter extends React.Component {
                 <span className="column column-50">
                     {tiles}
                 </span>
+                {gameInfoDisplay}
             </div>
         </div>;
     }
--- lib/memory/tile.ex
+++ lib/memory/tile.ex
@@ -4,7 +4,8 @@ defmodule Memory.Tile do
       tiles: generate_tiles(),
       active_tiles: [],
       completed_tiles: [],
-      clicks: 0
+      clicks: 0,
+      gameStatus: 0
     }
   end
 
@@ -13,17 +14,48 @@ defmodule Memory.Tile do
     tiles: tile.tiles,
     active_tiles: tile.active_tiles,
     completed_tiles: tile.completed_tiles,
-    clicks: tile.clicks
+    clicks: tile.clicks,
+    gameStatus: tile.gameStatus
   }
   end
 
+  def updated_tile_status(tiles, active_tiles, completed_tiles,tile_id) do
+    case active_tiles do
+        [id1]->
+            if Enum.at(tiles,id1-1) == Enum.at(tiles,tile_id-1) do
+                active_tiles = active_tiles ++ [tile_id]
+                completed_tiles = completed_tiles ++ active_tiles
+                {[],completed_tiles}
+            else
+                active_tiles = active_tiles ++ [tile_id]
+                {active_tiles,completed_tiles}
+            end
+        _ ->
+            active_tiles = [] ++ [tile_id]
+            {active_tiles,completed_tiles}
+    end
+  end
+
+  def get_gamestatus(completed_tiles) do
+    if length(completed_tiles)==16 do
+        2
+    else
+        1
+    end
+  end
+
   def select(tile,tile_id) do
+    tiles= tile.tiles
     active_tiles = tile.active_tiles
-    |> MapSet.new()
-    |> MapSet.put(tile_id)
-    |> MapSet.to_list
-    clicks = tile.clicks + 1
-     %{tile | clicks: clicks, active_tiles: active_tiles}
+    completed_tiles = tile.completed_tiles
+    if Enum.member?(active_tiles++completed_tiles, tile_id) do
+        %{tile | active_tiles: active_tiles, completed_tiles: completed_tiles}
+    else
+        {active_tiles,completed_tiles} = updated_tile_status(tiles, active_tiles, completed_tiles,tile_id)
+        clicks = tile.clicks + 1
+        gameStatus = get_gamestatus(completed_tiles)
+        %{tile | clicks: clicks, active_tiles: active_tiles, completed_tiles: completed_tiles, gameStatus: gameStatus}
+    end
   end
 
   def generate_tiles do
